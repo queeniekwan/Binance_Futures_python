420610dd4a541bcd1929f81c006716bff2aa1dcb
queeniekwan <60158228+queeniekwan@users.noreply.github.com>
2020-06-04 20:03:16 -0400

    test

--- example/market/get_24hr_ticker_price_change_statistics.py
+++ example/market/get_24hr_ticker_price_change_statistics.py
@@ -2,7 +2,7 @@ from binance_f import RequestClient
 from binance_f.constant.test import *
 from binance_f.base.printobject import *
 
-request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
+request_client = RequestClient(api_key=DKnzx80rRN3f7TTHcGbcOHUWj5RLIgkky2ta4Po3D10xOI9wzbjyQ3Ot817aRaqG, secret_key=CfNtVN2VjfMQju5FJOdPP8vQwglUMomQYet8CaCwth4zz9tElUyTDgxRjD8gPKO5)
 
 result = request_client.get_ticker_price_change_statistics()
 # result = request_client.get_ticker_price_change_statistics(symbol="BTCUSDT")
